705c811a63dd6e8412d08d2650f207d2c28544e9
diawara24 <mourtallafatydiawara@gmail.com>
2023-04-25 17:29:03 +0200

    ACTIVITE 17 : Rendre notre API Privée

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express();
 const woodCtrl = require('../controllers/wood.js');
+const auth = require("../middleware/auth.js")
 
-router.get('/all', woodCtrl.readAll);
+router.get('/all', auth, woodCtrl.readAll);
 
-router.get('/:hardness', woodCtrl.findByHardness);
+router.get('/:hardness', auth, woodCtrl.findByHardness);
 
 
 module.exports = router;
